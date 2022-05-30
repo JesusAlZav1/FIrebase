e1ebcfe3cd4a6e6598b9647266a26e3c24cc08b0
JesusAlZav1 <60371845+JesusAlZav1@users.noreply.github.com>
2022-05-30 18:09:09 -0500

    Initial commit

---
+++ app/src/main/java/com/jesus/firebase/MainActivity.java
@@ -0,0 +1,34 @@
+package com.jesus.firebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+    String token;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("FCm fail", "Fetching FCM registration token failed",
+                                    task.getException());
+                            return;
+                        }else{
+                            token = task.getResult();
+                            Log.d("FCM token", token);
+                        }
+                    }
+                });
+    }
+}
